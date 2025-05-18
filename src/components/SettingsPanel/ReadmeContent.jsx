bd30c1f81fb53ba0f2468e7f9a2a760605207b8b
atsukitanaka0922 <atsuki7660@gmail.com>
2025-05-18 20:33:12 +0900

    250518 commit

--- src/components/SettingsPanel/ReadmeContent.jsx
+++ src/components/SettingsPanel/ReadmeContent.jsx
@@ -159,7 +159,7 @@ function ReadmeContent() {
       
       <Section>
         <SectionTitle>バージョン情報</SectionTitle>
-        <p>おならじぇねれーたー v1.3.0</p>
+        <p>おならじぇねれーたー v1.3.1</p>
         <p>バグ報告や要望などはXアカウント@Onara_generatorまで</p>
         <p>© 2025 とあるフェチクリエイター</p>
       </Section>
